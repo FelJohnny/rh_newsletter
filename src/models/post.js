406f6cb2219ee44a9f3869b628e25a357cbb1c44
Otavio Souza <otaviosouzalu@gmail.com>
2024-03-05 21:52:45 -0300

    ajuste de modelos e linkando cada relacionamento, com belongsTo e hasMany

--- src/models/post.js
+++ src/models/post.js
@@ -1,26 +1,27 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Post.belongsTo(models.Tag, {
+        foreignKey: 'tag_id',
+      });
+      Post.belongsTo(models.Usuario, {
+        foreignKey: 'usuario_post_id',
+      });
     }
   }
-  Post.init({
-    titulo_post: DataTypes.STRING,
-    descricao_post: DataTypes.STRING,
-    anexo_post: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Post',
-    tableName: 'posts'
-  });
+  Post.init(
+    {
+      titulo_post: DataTypes.STRING,
+      descricao_post: DataTypes.STRING,
+      anexo_post: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Post',
+      tableName: 'posts',
+    },
+  );
   return Post;
 };
--- src/models/usuario.js
+++ src/models/usuario.js
@@ -2,13 +2,13 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Usuario extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Usuario.hasMany(models.Post, {
+        foreignKey: 'usuario_post_id',
+      });
+      Usuario.belongsTo(models.Rule, {
+        foreignKey: 'rule_id',
+      });
     }
   }
   Usuario.init(
