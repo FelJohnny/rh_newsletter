350f69a3e68204c506c2bca359fef5eeb9f9270c
unknown <otaviosouzalu@gmail.com>
2024-03-21 10:31:59 -0300

    ajuste de rotas de usuarios

--- src/routes/usuariosRoutes.js
+++ src/routes/usuariosRoutes.js
@@ -5,6 +5,10 @@ const usuarioController = new UsuarioController();
 
 const route = Router();
 
-route.get('/usuarios', (req, res) => usuarioController.pegaTodosController(req, res));
+route.get('/usuarios', (req, res) => {usuarioController.pegaTodosController(req, res)});
+route.get('/usuarios', (req, res) => {usuarioController.pegaTodosController(req, res)});
+route.get('/usuarios/:id', (req, res) => usuarioController.pegaUmRegistroPorIdController(req, res));
+route.put('/usuarios/:id', (req, res) => usuarioController.atulizaDadoController(req, res));
+route.delete('/usuarios/:id', (req, res) => usuarioController.excluiRegistroController(req, res));
 
 module.exports = route;
