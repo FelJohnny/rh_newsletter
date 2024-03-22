e26c3a0b83ca72b15d54d59b434d5768c396d255
otaviosouza21 <otaviosouzalu@gmail.com>
2024-03-21 22:20:10 -0300

    adicionando middleware multer para armazenar arquivos

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,18 +1,31 @@
-const { Router } = require('express');
-const PostController = require('../controllers/PostController.js');
-
+const { Router } = require("express");
+const PostController = require("../controllers/PostController.js");
+const multer = require("multer");
+const { storage } = require("../multerConfig.js");
+const upload = multer({ storage: storage });
 const postController = new PostController();
+const express = require("express");
 
 const route = Router();
 
-route.post('/posts', (req, res) => {postController.criaRegistroController(req, res)});
-route.get('/posts', (req, res) => {postController.pegaTodosController(req, res)});
-route.get('/posts/:id', (req, res) => postController.pegaUmRegistroPorIdController(req, res));
-route.put('/posts/:id', (req, res) => postController.atulizaDadoController(req, res));
-route.delete('/posts/:id', (req, res) => postController.excluiRegistroController(req, res));
-
-
-
-
+route.use("/files", express.static("uploads"));
+route.post("/upload", upload.single("file"), (req, res) => {
+  return res.json(req.file.filename);
+});
+route.post("/posts", (req, res) => {
+  postController.criaRegistroController(req, res);
+});
+route.get("/posts", (req, res) => {
+  postController.pegaTodosController(req, res);
+});
+route.get("/posts/:id", (req, res) =>
+  postController.pegaUmRegistroPorIdController(req, res)
+);
+route.put("/posts/:id", (req, res) =>
+  postController.atulizaDadoController(req, res)
+);
+route.delete("/posts/:id", (req, res) =>
+  postController.excluiRegistroController(req, res)
+);
 
 module.exports = route;
