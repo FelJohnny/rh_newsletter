25c3050e6cbe416b10eec309926cd37e07a57a7c
Felipe Johnny <felipijohnny@outlook.com>
2024-04-12 07:50:16 -0300

    aplicando correção de caminho de statico para imagens post

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ const path =require('path')
 const express = require('express');
 
 const app = require('./src/app.js')
+//necessario para que as imagens sejam acessiveis via caminho ex: 'http://localhost:3000/public/uploads/images/1712918282155_img.png'
+app.use('/public/uploads/images',express.static(path.join('public/uploads/images')))
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.static(path.join(__dirname,'public/uploads/images')))
-console.log(path.join(__dirname,'public'));
 const PORT = 3000
 app.listen(PORT,()=>{
    console.log('Servidor Escutando');
